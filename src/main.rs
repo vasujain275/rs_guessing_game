13e524f76e00d2611d6b9d74aa1a2ac6112af1d0
vasujain275 <vasujain275@gmail.com>
2024-03-27 19:34:22 +0530

    Completed the Guess Game

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::io;
 fn main() {
     println!("Guess a Number: ");
 
+    let mut count: i8 = 0;
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
@@ -16,15 +18,26 @@ fn main() {
 
         let guess: u32 = guess.trim().parse().expect("Please type of a number!");
 
-        println!("You Guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Small"),
-            Ordering::Greater => println!("Too Big"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+        if count < 10 {
+            match guess.cmp(&secret_number) {
+                Ordering::Less => {
+                    println!("Too Small");
+                    count = count + 1;
+                    println!("Only {} chances left!", 10 - count);
+                }
+                Ordering::Greater => {
+                    println!("Too Big");
+                    count = count + 1;
+                    println!("Only {} chances left!", 10 - count);
+                }
+                Ordering::Equal => {
+                    println!("You win!");
+                    println!("You took {} chances to win!", count);
+                    break;
+                }
             }
+        } else {
+            print!("You Loose!")
         }
     }
 }
